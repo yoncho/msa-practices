a627930a19f5cbbb73bf42afd307513801d3f4c5
yoncho <joyonghyeon13@gmail.com>
2023-11-15 10:54:03 +0900

    study - msa emaillist08

--- clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
+++ clients/emaillist08/backend/src/main/java/com/poscodx/emaillist/exception/GlobalExceptionHandler.java
@@ -1,16 +1,14 @@
-package com.poscodx.msa.service.emaillist.exception;
+package com.poscodx.emaillist.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.NoHandlerFoundException;
-
-import com.poscodx.msa.service.emaillist.dto.JsonResult;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,21 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
-	@ExceptionHandler(NoHandlerFoundException.class)
-	public String NoHandlerFoundException(Exception e) {
-		// 응답
-		return "index";
-	}
-	
 	@ResponseBody
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> handlerException(Exception e) {		
-		// 로깅(Logging)
+	public ResponseEntity<?> handlerException(Exception e) {
+		// 0. 로깅(Logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
-
+		
 		// 응답
-		return ResponseEntity.status(HttpStatus.OK).body(JsonResult.fail(errors.toString()));
+		return ResponseEntity.status(HttpStatus.OK).body(Map.of("error", errors.toString()));	
 	}
 }
